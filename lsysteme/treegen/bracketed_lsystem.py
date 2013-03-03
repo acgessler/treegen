a2600d22c92232674462d6da1eed1dd004f2bd5e
Alexander Gessler <alexander.gessler@gmx.net>
2013-03-03 16:36:50 +0100

    - add bracketed_lsystem.py, which retains alphabet symbols with no productions.

--- lsysteme/treegen/bracketed_lsystem.py
+++ lsysteme/treegen/bracketed_lsystem.py
@@ -1,11 +1,10 @@
 
 
-class lsystem:
-    """Simple, deterministic Lindenmayer system (D0)
-    
-    Takes a deterministic, context free grammar and repeatedly applies
-    it to a given axiom, forming self-similar structures. It is expected
-    that there are deduction rules for all alphabet symbols.
+from lsystem import lsystem
+
+class bracketed_lsystem(lsystem):
+    """Bracketed Lindenmayer system. Same as lystem class except that
+    it retains alphabet symbols for which there are no productions.
     """
 
     def __init__(self, rules, axiom):
@@ -20,19 +19,16 @@ class lsystem:
         axiom:
             axiom to start with
         """
-        self.rules = rules
-        self.axiom = axiom
-
-        assert(isinstance(self.rules,dict) and len(self.rules))
-        assert(isinstance(self.axiom,str))
-
-
+        lsystem.__init__(self, rules, axiom)
+       
+        assert(not '[' in self.rules.keys())
+        assert(not ']' in self.rules.keys())
 
     def evaluate(self, n):
         """
         Return axiom evaluated after n iterations
         """
-        assert(iterations >= 0)
+        assert(n >= 0)
 
         return self._rec_eval(n, self.axiom)
 
@@ -42,13 +38,16 @@ class lsystem:
 
         out = []
         for a in word:
-            out.append(self._rec_eval(iterations-1,self.rules[a]))
+            if not a in self.rules:
+                out.append(a)      
+            else:
+                out.append(self._rec_eval(iterations-1,self.rules[a]))
 
         return ''.join(out)
 
 
 def test():
-    ls = lsystem( { 'a' : 'ab', 'b' : 'a'},'b')
+    ls = bracketed_lsystem( { 'F': 'FF-[-F+F+F]+[+F-F-F]'},'F')
     print(ls.evaluate(2))
 
 if __name__ == '__main__':
