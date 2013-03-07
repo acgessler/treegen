cab6e5525067931f14c4527d941911400c89b7ed
Alexander Gessler <alexander.gessler@gmx.net>
2013-03-07 15:27:19 +0100

    - more grammar experiments, add raw orbit camera.

--- lsysteme/treegen/stochastic_tropism_turtle3d_test.py
+++ lsysteme/treegen/stochastic_tropism_turtle3d_test.py
@@ -12,10 +12,15 @@ from stochastic_lsystem import stochastic_lsystem
 
 import math
 
-d = 1
-angles = (22,22,22)
+d = 10
+angles = (22.5,22.5,22.5)
 trop = (0.2,-0.5,0)
 e = 0.0
+
+# global vars for camera rotation 
+heading = 0 
+pitch = 0 
+
  
 class TestApp(ShowBase):
     def __init__(self):
@@ -27,22 +32,20 @@ class TestApp(ShowBase):
 
         base.setBackgroundColor(0.0, 0.0, 0.0) 
         base.disableMouse()
-        camera.setPos(0.0, 0.0, 20.0) 
-        camera.setHpr(0.0, -90.0, 0.0) 
 
         vdata = GeomVertexData('name', GeomVertexFormat.getV3(), Geom.UHStatic)
 
         ls = stochastic_lsystem([
-            ('A',   r'[&FLA]/////[&FLA]///////[&FLA]',              1),
-            ('F',   r'S/////F',                                     1),
-            ('S',   r'F',                                           1),
-            ('L',   r'[F^^F][F\\\\F]',                              1)], 
+            ('A',   r'[&FL!A]/////[&FL!A]///////[&FL!A]',               1),
+            ('F',   r'S/////F',                                         1),
+            ('S',   r'F L',                                             1),
+            ('L',   r'[^^-f+f+f-|-f+f+f]',                              1)], 
         'A' )
 
         turtle = tropism_turtle3d(d,angles,trop,e)
-        evaluated = ls.evaluate(5)
+        evaluated = ls.evaluate(6)
         #print evaluated
-        lines = turtle.get_3d_lines(evaluated,(0,-2,0), start_forward=(0,1,0), start_right=(1,0,0)) 
+        lines = turtle.get_3d_lines(evaluated,(0,-2,0), start_forward=(0,0,1), start_right=(1,0,0)) 
         #print lines
 
         vertex = GeomVertexWriter(vdata, 'vertex')
@@ -63,11 +66,49 @@ class TestApp(ShowBase):
      
         nodePath = render.attachNewNode(node)
         nodePath.setPos(0,0,0)
-       
+
+
+        # orbit camera code taken from
+        # http://www.panda3d.org/forums/viewtopic.php?t=9292
+
+        # dummy node for camera, we will rotate the dummy node fro camera rotation 
+        parentnode = render.attachNewNode('camparent') 
+        parentnode.reparentTo(nodePath) # inherit transforms 
+        parentnode.setEffect(CompassEffect.make(render)) # NOT inherit rotation 
+
+        # the camera 
+        base.camera.reparentTo(parentnode) 
+        base.camera.lookAt(parentnode) 
+        base.camera.setY(-30) # camera distance from model 
+
+        # camera zooming 
+        base.accept('wheel_up', lambda : base.camera.setY(base.camera.getY()+200 * globalClock.getDt())) 
+        base.accept('wheel_down', lambda : base.camera.setY(base.camera.getY()-200 * globalClock.getDt())) 
+
+        # camera rotation task 
+        def thirdPersonCameraTask(task): 
+           global heading 
+           global pitch 
+    
+           md = base.win.getPointer(0) 
+      
+           x = md.getX() 
+           y = md.getY() 
+    
+           if base.win.movePointer(0, 300, 300): 
+              heading = heading - (x - 300) * 0.5 
+              pitch = pitch - (y - 300) * 0.5 
+    
+           parentnode.setHpr(heading, pitch,0) 
+           return task.cont 
+
+        taskMgr.add(thirdPersonCameraTask, 'thirdPersonCameraTask')      
+
+        # set dummy material
         myMaterial = Material()
         myMaterial.setShininess(5.0) 
         myMaterial.setAmbient(VBase4(0,1,0,1)) 
-        myMaterial.setDiffuse(VBase4(1,1,1,1))
+        myMaterial.setDiffuse(VBase4(0.2,1,0.5,1))
         nodePath.setMaterial(myMaterial)
         nodePath.setTwoSided(True)
 
--- lsysteme/treegen/tropism_turtle3d.py
+++ lsysteme/treegen/tropism_turtle3d.py
@@ -9,6 +9,7 @@ class tropism_turtle3d:
     +,-     Yaw
     &,^     Pitch
     \/      Roll
+    |       Flip (yaw by 180deg)
 
     F       Move forward
     """
@@ -79,7 +80,7 @@ class tropism_turtle3d:
         start_right = Vec3(*start_right) \
             if not isinstance(start, Vec3) else start_right
 
-        start_up = start_forward.cross(start_right)
+        start_up = -start_forward.cross(start_right)
 
         all_nonzero = start_forward.normalize() and start_up.normalize() and start_right.normalize()
         assert all_nonzero
@@ -94,7 +95,7 @@ class tropism_turtle3d:
 
         # interpret the program, collecting lines in [output]
         total = self._rec_eval(program, 0, start, quat, output)
-        #assert(total == len(program))
+        assert(total == len(program))
 
         return output
 
@@ -102,29 +103,31 @@ class tropism_turtle3d:
     def _rec_eval(self, program, cursor, start, quat, output):
         lstart = Point3(start)
         lquat = LOrientationf(quat)
+        lquat.normalize() # normalize the quat each time to prevent numerical errors
 
         n = cursor
         processed = 0
         while n < len(program):
             processed += 1
             c = program[n]
-            if c == 'F':          
-                newPoint = self._compute_next(lstart, lquat) 
+            if c == 'F' or c == 'f':          
+                newPoint = self._compute_next(lstart, lquat, c=='f') 
                 output.append((lstart,newPoint))
-                lstart = newPoint
                 lstart = newPoint
             elif c == '-':
                 lquat *= LRotationf(Vec3(0,1,0),-self.angles[2])
             elif c == '+':
                 lquat *= LRotationf(Vec3(0,1,0),self.angles[2])
             elif c == '\\':
-                lquat *= LRotationf(Vec3(0,0,1),-self.angles[0])
+                lquat *= LRotationf(Vec3(0,0,1), self.angles[0])
             elif c == '/':
-                lquat *= LRotationf(Vec3(0,0,1),self.angles[0])
+                lquat *= LRotationf(Vec3(0,0,1),-self.angles[0])
             elif c == '&':
-                lquat *= LRotationf(Vec3(1,0,0),-self.angles[1])
-            elif c == '^':
                 lquat *= LRotationf(Vec3(1,0,0),self.angles[1])
+            elif c == '^':
+                lquat *= LRotationf(Vec3(1,0,0),-self.angles[1])
+            elif c == '|':
+                lquat *= LRotationf(Vec3(0,1,0),180)
             elif c == '[':
                 cnt = self._rec_eval(program, n + 1, lstart, lquat, output) 
                 n += cnt
@@ -140,10 +143,10 @@ class tropism_turtle3d:
         return processed
 
 
-    def _compute_next(self, lstart, lquat):
-        dir = lquat.getForward() + self.trop * self.e
+    def _compute_next(self, lstart, lquat, small):
+        dir = lquat.getUp() + self.trop * self.e
         dir.normalize()
-        return lstart + dir * self.d     
+        return lstart + dir * self.d * (0.1 if small else 1.0)
         
    
 
